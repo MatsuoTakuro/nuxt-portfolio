c02efd0d11b953e7189dd527a1db0e8e517d9eb4
taro <takuro.matsuo@yappli.co.jp>
2025-07-22 07:18:20 +0900

    feat: initial commit

---
+++ pages/blog.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+        <h1>Blog</h1>
+        <p>Sharing my everyday life as an engineer.</p>
+        <div v-for="singleData in data" :key="singleData.id">
+            <div>
+                <h3>{{ singleData.title }}</h3>
+                <p>{{ singleData.excerpt }}</p>
+                <p>{{ singleData.meta.date }}</p>
+            </div>
+            <div>
+                <nuxt-img src="singleData.meta.image" alt="blog-image" format="webp" />
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("blogQuery", () =>
+    queryCollection("blog").all()
+)
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to My Portfolio</h1>
+    <p>This is the homepage of my Nuxt.js portfolio.</p>
+  </div>
+</template>
